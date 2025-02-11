2fa2ab85c7b5a9a74ef47d4fc618f3d0457fdacc
yuki <SaigyoujiNexas@gmail.com>
2025-02-11 23:19:50 +0800

    feat: delete useless plugin

--- deploy.sh
+++ deploy.sh
@@ -11,18 +11,15 @@ echo "common dotfiles deploy success."
 
 #unix deploy
 if [[ "$os_name" == "Linux" || "$os_name" == "Darwin" || "$os_name" == "FreeBSD" ]]; then
-  stow --adopt --dotfiles unix -t ~
-  echo "unix dotfiles deploy success."
+  stow --adopt --dotfiles unix -t ~ && echo "unix dotfiles deploy success."
 fi
 
 if [[ "$os_name" == "Linux" ]]; then
   #linux deploy
-  stow --adopt --dotfiles linux -t ~
-  echo "linux dotfiles deploy success."
+  stow --adopt --dotfiles linux -t ~ && echo "linux dotfiles deploy success."
 elif [[ "$os_name" == "Darwin" ]]; then
   #MacOS deploy
-  stow --adopt --dotfiles macos -t ~
-  echo "macos dotfiles deploy success."
+  stow --adopt --dotfiles macos -t ~ && echo "macos dotfiles deploy success."
 fi
 
 cd "$origin_dir" || exit 1
