6ee526fc805029959c4fdf17d4350d98a3d18339
lixinyan10 <lixinyan10@meituan.com>
2025-02-20 14:28:07 +0800

    refact: rewrite command

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,10 @@ origin_dir=$(pwd)
 work_dir=$(dirname "${BASH_SOURCE[0]}")
 
 declare -A deploy_map
-declare -a order_list
 deploy_map=([common]="Linux Darwin FreeBSD" [unix]="Linux Darwin FreeBSD" [linux]="Linux" [macos]="Darwin")
+declare -a order_list
 order_list=("common" "unix" "linux" "macos")
+ignorefiles=("YukiConfig.code-profile")
 os_name=$(uname)
 
 cd "$work_dir" || exit 1
@@ -22,7 +23,13 @@ for target in "${order_list[@]}"; do
 	read -r -a support_os <<<"${deploy_map[$target]}"
 	for support_os_item in "${support_os[@]}"; do
 		if [[ "$support_os_item" == "$os_name" ]]; then
-			stow --adopt --dotfiles "$target" -t ~ && echo "$target" dotfiles deploy success.
+			ignore_command=""
+
+			for ignore in "${ignorefiles[@]}"; do
+				ignore_command="--ignore=$ignore $ignore_command"
+			done
+			command="stow --adopt --dotfiles $target $ignore_command -t $HOME"
+			$command && echo "$target dotfiles deploy success." || echo "$target dotfiles deploy failed."
 		fi
 	done
 done
