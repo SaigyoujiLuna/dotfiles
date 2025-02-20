a4816a8ba373f9f5aaa65c3168c9bf7049b0ffcb
lixinyan10 <lixinyan10@meituan.com>
2025-02-20 14:03:19 +0800

    refact: rewrite deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,49 @@
+#!/usr/bin/env bash
+
+# environment check
+if ! command -v stow >/dev/null 2>&1; then
+	echo "stow is not available"
+	echo "Please install stow first"
+	exit 1
+fi
+
 origin_dir=$(pwd)
 work_dir=$(dirname "${BASH_SOURCE[0]}")
 
+declare -A deploy_map
+declare -a order_list
+deploy_map=([common]="Linux Darwin FreeBSD" [unix]="Linux Darwin FreeBSD" [linux]="Linux" [macos]="Darwin")
+order_list=("common" "unix" "linux" "macos")
+os_name=$(uname)
+
 cd "$work_dir" || exit 1
 
-os_name=$(uname)
-stow --adopt --dotfiles common -t ~
+for target in "${order_list[@]}"; do
+	read -r -a support_os <<<"${deploy_map[$target]}"
+	for support_os_item in "${support_os[@]}"; do
+		if [[ "$support_os_item" == "$os_name" ]]; then
+			stow --adopt --dotfiles "$target" -t ~ && echo "$target" dotfiles deploy success.
+		fi
+	done
+done
 
-# common config deploy
-echo "common dotfiles deploy success."
-
-#unix deplo
-if [[ "$os_name" == "Linux" || "$os_name" == "Darwin" || "$os_name" == "FreeBSD" ]]; then
-  stow --adopt --dotfiles unix --ignore=YukiConfig.code-profile -t ~ && echo "unix dotfiles deploy success."
-fi
-
-if [[ "$os_name" == "Linux" ]]; then
-  #linux deploy
-  stow --adopt --dotfiles linux -t ~ && echo "linux dotfiles deploy success."
-elif [[ "$os_name" == "Darwin" ]]; then
-  #MacOS deploy
-  stow --adopt --dotfiles macos -t ~ && echo "macos dotfiles deploy success."
-fi
+# stow --adopt --dotfiles common --ignore=YukiConfig.code-profile -t ~
+#
+# # common config deploy
+# echo "common dotfiles deploy success."
+#
+# #unix deplo
+# if [[ "$os_name" == "Linux" || "$os_name" == "Darwin" || "$os_name" == "FreeBSD" ]]; then
+# 	stow --adopt --dotfiles unix --ignore=YukiConfig.code-profile -t ~ && echo "unix dotfiles deploy success."
+# fi
+#
+# if [[ "$os_name" == "Linux" ]]; then
+# 	#linux deploy
+# 	stow --adopt --dotfiles linux -t ~ && echo "linux dotfiles deploy success."
+# elif [[ "$os_name" == "Darwin" ]]; then
+# 	#MacOS deploy
+# 	stow --adopt --dotfiles macos -t ~ && echo "macos dotfiles deploy success."
+# fi
 
 cd "$origin_dir" || exit 1
 echo "All done, Ciallo ～(∠・ω< )⌒★"
