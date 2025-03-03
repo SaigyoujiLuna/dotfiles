7120b573435881607c07a645c5ea0aa246dbd198
lixinyan10 <lixinyan10@meituan.com>
2025-03-03 13:50:10 +0800

    refactor: change neovim setting

--- unix/dot-config/nvim/lua/plugins/cmp/core.lua
+++ unix/dot-config/nvim/lua/plugins/cmp/core.lua
@@ -16,13 +16,28 @@ return {
         list = {
           selection = {
             preselect = false,
-            auto_insert = true,
+            auto_insert = false,
           },
         },
+        trigger = {
+          show_in_snippet = false
+        }
       },
       signature = { window = { border = "rounded" } },
       keymap = {
         preset = "enter",
+        -- ["<Tab>"] = {
+        --   function(cmp)
+        --     if cmp.snippet_active() then
+        --       return cmp.accept()
+        --     else
+        --       return cmp.select_and_accept()
+        --     end
+        --   end,
+        --   "snippet_forward",
+        --   "fallback",
+        -- },
+        -- ["<C-d>"] = { "scroll_docs", "-4" }, 
         ["<Up>"] = { "select_prev", "fallback" },
         ["<Down>"] = { "select_next", "fallback" },
         ["<Tab>"] = { "select_next", "fallback" },
