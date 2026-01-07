c9be582b410ac908aaa4298dba44fe47b0adfe19
yuki <saigyoujiluna@gmail.com>
2026-01-07 16:58:22 +0800

    feat: neovide disown

--- unix/dot-config/nvim/lua/config/keymaps/normal_keymaps.lua
+++ unix/dot-config/nvim/lua/config/keymaps/normal_keymaps.lua
@@ -17,7 +17,15 @@ keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true }
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
+-- clipboard support
+keymap.set('n', '<D-s>', ':w<CR>') -- Save
+keymap.set('v', '<D-c>', '"+y') -- Copy
+keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
 --ui toggle
 Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
 Snacks.toggle.zen():map("<leader>uz")
 Snacks.toggle.dim():map("<leader>ud")
+
