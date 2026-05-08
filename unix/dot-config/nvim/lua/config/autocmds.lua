303ccde0cb266564c0389708d932e3da67002578
yuki <saigyoujiluna@gmail.com>
2026-05-08 15:09:58 +0800

    feat: neovim option opt

--- unix/dot-config/nvim/lua/config/autocmds.lua
+++ unix/dot-config/nvim/lua/config/autocmds.lua
@@ -4,4 +4,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
-
--- unix/dot-config/nvim/lua/config/options.lua
+++ unix/dot-config/nvim/lua/config/options.lua
@@ -1,17 +1,31 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 local opt = vim.opt
-opt.relativenumber = true
+
 opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.wrap = false
+opt.scrolloff = 8
+opt.sidescrolloff = 8
 
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
+opt.smartindent = true -- smart auto indent
 opt.autoindent = true
 
-opt.wrap = false
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+
+opt.signcolumn = "yes"
+-- opt.colorcolumn = "100" -- show column at 100 position chars
+opt.showmatch = true
+opt.showmode = false
+
 opt.confirm = true
-opt.cursorline = true
 
 opt.mouse:append("a")
 opt.clipboard:append("unnamedplus")
@@ -19,11 +33,7 @@ opt.clipboard:append("unnamedplus")
 opt.splitright = true
 opt.splitbelow = true
 
-opt.ignorecase = true
-opt.smartcase = true
 
 opt.termguicolors = true
-opt.signcolumn = "yes"
 
-opt.scrolloff = 8
 opt.smoothscroll = true
